57e9da348e4e3385fc93cd84c24b89a5b88dddbc
MRbcgd <quittypig@gmail.com>
2017-01-18 13:38:52 +0900

    morgan, compression, body-parser, method-override config/env ->환경에 관련된 파일들 config.js->개발자, 사용자에 따라 config.env의 코드를 불러옴

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//개발자환경, 사용자환경으로 수정하고 싶다면 'production'으로 수정한다.
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var express = require('./config/express_config.js');
 var app = express();
 
--- config/express_config.js
+++ config/express_config.js
@@ -1,7 +1,24 @@
-var express=require('express')
+var express=require('express'),
+    morgan=require('morgan'),//LOGGER 사용위해
+    compression=require('compression'),//응답데이터 압축
+    bodyParser=require('body-parser'),//요청 데이터처리
+    methodOverride=require('method-override')//PUT,DELETE등 HTTP 동사 지원
 
 module.exports=function(){
   var app=express();
+
+  if(process.env.NODE_ENV === 'development') {//개발자 환경이라면
+        app.use(morgan('dev'));
+    } else if (process.env.NODE_ENV === 'production') {//사용자 환경이라면
+        app.use(compress());
+    }
+
+  app.use(bodyParser.urlencoded({//body-parser 사용 위해
+      extended : true
+  }));
+  app.use(bodyParser.json());//JSON 사용위해
+  app.use(methodOverride());//method-override 사용 위해
+
   require('../app/routes/index.server.routes.js')(app)
   return app;
 }
