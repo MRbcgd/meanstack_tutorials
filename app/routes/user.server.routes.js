d3d2f63d81398d32ff1b69c28efc5423481af049
MRbcgd <quittypig@gmail.com>
2017-01-18 16:02:57 +0900

    fine(),findOne() update(),findOneAndUpdate() ,findByIdAndUpdate() remove() , findOneAndRemove(), findByIdAndRemove()

--- app/routes/user.server.routes.js
+++ app/routes/user.server.routes.js
@@ -2,4 +2,10 @@ var users=require('../controllers/user.server.controller.js')
 
 module.exports=function(app){//users url로 접속시 post하면 user테이블이 생성된다
   app.route('/users').post(users.create).get(users.list)
+
+  app.route('/users/:userId')
+  .get(users.read)
+  .put(users.update)
+  .delete(users.delete)
+  app.param('userId',users.userByID)
 }
